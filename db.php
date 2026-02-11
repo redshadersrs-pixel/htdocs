96aa523de6c2c615c7136367565fd1a4bb4e2191
Ahmed Lemghaili <2240846@talnet.nl>
2026-02-11 16:16:37 +0100

    db.php commit

---
+++ db.php
@@ -0,0 +1,22 @@
+<?php
+
+$host = 'localhost';
+$db   = 'winkel';
+$user = 'root';
+$pass = 'root';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+try 
+{
+     $pdo = new PDO($dsn, $user, $pass, $options);
+     echo "Connectie gemaakt!";
+} 
+catch (\PDOException $e) 
+{
+     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
